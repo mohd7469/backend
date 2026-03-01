e8cf569f03f3c66ef9718ee77f1eb75bf3c98c7a
Awais Karni <awais3227443@gmail.com>
2026-03-01 12:34:08 +0000

    .

--- src/server.js
+++ src/server.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import cors from 'cors';
+import helmet from 'helmet';
 
 import routes from './routes/index.js';
 import { errorHandler } from './middlewares/index.js';
 import { env } from './config/env.js';
 
 const app = express();
+
+// security middlewares
+app.use(helmet());
 
 app.use(cors());
 app.use(express.json());
